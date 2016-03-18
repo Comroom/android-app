66cd15bad7ee286cf7d663c46c08200abea9dc90
InhoChoi <revaer@naver.com>
2016-03-18 17:29:06 +0900

    필요없는 import 삭제

--- app/src/main/java/net/comroom/comroombook/MainActivity.java
+++ app/src/main/java/net/comroom/comroombook/MainActivity.java
@@ -2,14 +2,6 @@ package net.comroom.comroombook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
