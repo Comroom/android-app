3185363740ae16853295fb9f6919455ea86ad152
InhoChoi <revaer@naver.com>
2016-03-28 17:22:47 +0900

    Login UI 제작

---
+++ app/src/main/java/net/comroom/comroombook/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package net.comroom.comroombook.ui;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import net.comroom.comroombook.R;
+
+public class LoginActivity extends Activity  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+}
--- app/src/main/java/net/comroom/comroombook/ui/MainActivity.java
+++ app/src/main/java/net/comroom/comroombook/ui/MainActivity.java
@@ -1,5 +1,6 @@
 package net.comroom.comroombook.ui;
 
+import android.content.Intent;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.design.widget.NavigationView;
@@ -7,6 +8,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 import net.comroom.comroombook.R;
@@ -30,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
         setupNavigationView();
         setupToolbar();
         setupFragments();
+
+        /* Login Page */
+        startActivity(new Intent(MainActivity.this,LoginActivity.class));
     }
 
     @Override
