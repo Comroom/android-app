219bf9ec84282803e23144625aae110e0d713f6d
donnaknew <donnaknew@gmail.com>
2016-03-29 17:45:00 +0900

    비밀번호 재입력에서 일치하는지 체크 애니메이션 추가

--- app/src/main/java/net/comroom/comroombook/ui/SignUpActivity.java
+++ app/src/main/java/net/comroom/comroombook/ui/SignUpActivity.java
@@ -5,6 +5,8 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +16,7 @@ import com.loopj.android.http.AsyncHttpResponseHandler;
 
 import net.comroom.comroombook.R;
 import net.comroom.comroombook.core.ComroomRestClient;
+import net.comroom.comroombook.ui.signup_ui.CheckMarkView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -29,7 +32,15 @@ public class SignUpActivity extends Activity{
     EditText et_name;
     EditText et_email;
     EditText et_password;
+    EditText et_checkpwd;
     Button bt_signUp;
+
+    String buffName = null;
+    String buffEmail = null;
+    String buffPasswordd = null;
+    String buffCheckPassword = null;
+
+    CheckMarkView checkPassword;
 
     private ProgressDialog progressDialog;
 
@@ -38,9 +49,13 @@ public class SignUpActivity extends Activity{
         public void onClick(View v) {
             switch (v.getId()){
                 case R.id.signup_signup:
-                    bt_signUp.setEnabled(false);
-                    signUp();
-                    bt_signUp.setEnabled(true);
+                    if(checkPassword.isChecked()){
+                        bt_signUp.setEnabled(false);
+                        signUp();
+                        bt_signUp.setEnabled(true);
+                    }else{
+                        Toast.makeText(SignUpActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                    }
                     break;
             }
         }
@@ -58,14 +73,62 @@ public class SignUpActivity extends Activity{
         et_name = (EditText)findViewById(R.id.signup_name);
         et_email = (EditText)findViewById(R.id.signup_email);
         et_password = (EditText)findViewById(R.id.signup_password);
+        et_checkpwd = (EditText)findViewById(R.id.signup_checkpwd);
         bt_signUp = (Button)findViewById(R.id.signup_signup);
+
+        checkPassword = (CheckMarkView)findViewById(R.id.check_mark_view);
+        checkPassword.setVisibility(View.INVISIBLE);
+
+        et_password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                buffPasswordd = et_password.getText().toString();
+                if(buffCheckPassword != null && (!buffPasswordd.equals(buffCheckPassword))){
+                    checkPassword.setChecked(true);
+                    checkPassword.toggle();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        et_checkpwd.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                buffPasswordd = et_password.getText().toString();
+                buffCheckPassword = et_checkpwd.getText().toString();
+                if(checkPassword.getVisibility() == View.INVISIBLE) {
+                    checkPassword.setVisibility(View.VISIBLE);
+                }
+                if(buffCheckPassword.equals("")){
+                    checkPassword.setVisibility(View.INVISIBLE);
+                }else if(buffPasswordd.equals(buffCheckPassword)){
+                    checkPassword.setChecked(false);
+                    checkPassword.toggle();
+                }else if(!buffPasswordd.equals(buffCheckPassword)){
+                    checkPassword.setChecked(true);
+                    checkPassword.toggle();
+                }
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
         bt_signUp.setOnClickListener(clickListener);
     }
 
     private void signUp(){
-        String buffName = et_name.getText().toString();
-        String buffEmail = et_email.getText().toString();
-        String buffPasswordd = et_password.getText().toString();
+        buffName = et_name.getText().toString();
+        buffEmail = et_email.getText().toString();
+        buffPasswordd = et_password.getText().toString();
+        buffCheckPassword = et_checkpwd.getText().toString();
 
         if(buffName.length() == 0 || buffEmail.length() == 0 || buffPasswordd.length() == 0){
             Toast.makeText(SignUpActivity.this, "회원가입 정보를 입력해주세요.", Toast.LENGTH_SHORT).show();
