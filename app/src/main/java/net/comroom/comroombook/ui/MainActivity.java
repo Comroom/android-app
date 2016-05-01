a4ce6e8209b0b044686115f19e3c9e55b5015eb7
donnaknew <donnaknew@gmail.com>
2016-05-01 20:45:53 +0900

    채팅방 만들 버튼생성

--- app/src/main/java/net/comroom/comroombook/ui/MainActivity.java
+++ app/src/main/java/net/comroom/comroombook/ui/MainActivity.java
@@ -1,6 +1,8 @@
 package net.comroom.comroombook.ui;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -11,12 +13,20 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import com.github.clans.fab.FloatingActionButton;
+import com.github.clans.fab.FloatingActionMenu;
+
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import net.comroom.comroombook.R;
 import net.comroom.comroombook.ui.FragmentMain;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
 
     private DrawerLayout drawerLayout;
     private NavigationView navigation;
@@ -26,18 +36,48 @@ public class MainActivity extends AppCompatActivity {
     private FragmentTimeTable timeTableFragment;
     private FragmentMessage messageFragment;
 
+    private FloatingActionMenu faMenu;
+    private FloatingActionButton fabCreateChat;
+    private Handler mUiHandler = new Handler();
+
     static final int REQUSET_LOGIN = 1;
     static final int RESULT_LOGIN = 2;
     public static String user_id;
+
+    private View.OnTouchListener fabActionListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            return false;
+        }
+    };
+
+    private View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.fab_create_chat:
+                    break;
+            }
+            Log.d(TAG, "채팅방 개설");
+            Snackbar
+                    .make(findViewById(R.id.coordinatelayout), "채팅방이 개설됐습니다.", Snackbar.LENGTH_SHORT)
+                    .setAction("Action", this)
+                    .show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initFabComponents();
+        initFabActionListner();
+
         setupNavigationView();
         setupToolbar();
         setupFragments();
+        setupFab();
 
         /* Login Page */
         startActivityForResult(new Intent(MainActivity.this, LoginActivity.class), REQUSET_LOGIN);
@@ -75,18 +115,23 @@ public class MainActivity extends AppCompatActivity {
                 int itemid = menuItem.getItemId();
                 switch (itemid) {
                     case R.id.navigation_item_0:
+                        setFabVisibilityGone();
                         getSupportFragmentManager().beginTransaction().replace(R.id.mainfc, mainFragment).commit();
                         break;
                     case R.id.navigation_item_1:
+                        setFabVisibility();
                         getSupportFragmentManager().beginTransaction().replace(R.id.mainfc, chatFragment).commit();
                         break;
                     case R.id.navigation_item_2:
+                        setFabVisibilityGone();
                         getSupportFragmentManager().beginTransaction().replace(R.id.mainfc, timeTableFragment).commit();
                         break;
                     case R.id.navigation_item_3:
+                        setFabVisibilityGone();
                         getSupportFragmentManager().beginTransaction().replace(R.id.mainfc, messageFragment).commit();
                         break;
                     case R.id.navigation_time_4:
+                        setFabVisibilityGone();
                         startActivityForResult(new Intent(MainActivity.this, LoginActivity.class), REQUSET_LOGIN);
                         getSupportFragmentManager().beginTransaction().replace(R.id.mainfc, new Fragment()).commit();
                         break;
@@ -103,6 +148,45 @@ public class MainActivity extends AppCompatActivity {
         timeTableFragment = new FragmentTimeTable();
         messageFragment = new FragmentMessage();
         //getSupportFragmentManager().beginTransaction().replace(R.id.mainfc, mainFragment).commit();
+    }
+
+    private void initFabComponents(){
+        faMenu = (FloatingActionMenu) findViewById(R.id.fab_menu);
+        fabCreateChat = (FloatingActionButton)findViewById(R.id.fab_create_chat);
+    }
+
+    private void initFabActionListner(){
+        fabCreateChat.setOnClickListener(clickListener);
+    }
+
+    private void setupFab() {
+        faMenu.setVisibility(View.GONE);
+        faMenu.setOnMenuButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (faMenu.isOpened()) {
+                    Toast.makeText(MainActivity.this, faMenu.getMenuButtonLabelText(), Toast.LENGTH_SHORT).show();
+                }
+
+                faMenu.toggle(true);
+            }
+        });
+        faMenu.hideMenuButton(false);
+        mUiHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                faMenu.showMenuButton(true);
+            }
+        }, 150);
+        faMenu.setClosedOnTouchOutside(true);
+    }
+
+    public void setFabVisibility(){
+        faMenu.setVisibility(View.VISIBLE);
+    }
+
+    public void setFabVisibilityGone(){
+        faMenu.setVisibility(View.GONE);
     }
 
     private void setupToolbar() {
